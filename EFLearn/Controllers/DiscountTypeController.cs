a35f88d7f159ebfeb98fc9b96b9d5c9dd7ceda60
Mateen Memon <Mateen.m@illusions-online.com>
2018-02-15 12:47:58 +0400

    Initial commit

---
+++ EFLearn/Controllers/DiscountTypeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EFLearn.Controllers
+{
+    public class DiscountTypeController : Controller
+    {
+
+        // GET: DiscountType
+        public ActionResult Index()
+        {
+            var _context = new TestDBContext();            
+            var res = _context.M_DiscountType.ToList();
+
+            return View(res);
+        }
+        public ActionResult Hello()
+        {
+            return View();
+        }
+        public ActionResult Hello1()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ EFLearn/ViewModel/clstest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace EFLearn.ViewModel
+{
+    public class clstest
+    {
+        public List<M_DiscountType> discTypes { get; set; }
+
+        [Required]
+        public string strRequired { get; set; }
+    }
+}
